427bc35c7e4bbd96c21f0b6b476ab011362607f8
jatinder singh sandhu <jatindersandhu.it@gmail.com>
2019-09-14 20:30:15 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -82,6 +82,18 @@ def call() {
                    } 
                 }
             }
+             stage('send_input_start')
+             {
+                 jobtype = "build"
+                 branch = "dev"
+                 application = "My Application"
+                 component = "My Component"
+                 release = "My Release"
+                 jstatus = "Started"
+                 jresult = "Building"
+                 msg = "BNum="+BUILD_NUMBER+",JName="+JOB_NAME+",AppName="+application+",CompName="+component+",BranchName="+branch+",RelNumber="+release+",JobStatus="+jstatus+",JobResult="+jresult+",JType="+jobtype
+                 splunkins.send(msg)
+             }
         }
         post {
             always {
